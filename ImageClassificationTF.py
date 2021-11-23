13a9d07d8c6f0ec1065e585568bd6ed34af09cf1
nahplay <46632366+nahplay@users.noreply.github.com>
2021-11-23 12:08:02 +0200

    Update ImageClassificationTF.py

--- ImageClassificationTF.py
+++ ImageClassificationTF.py
@@ -12,16 +12,6 @@ from keras_lr_finder import LRFinder
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Flatten, Dropout, AveragePooling2D
 
-'''
-Here is the implementation of VGG16, VGG19 - with Keras and
-Resnet50 and Densenet161 with Pytorch. Layers of base model were set to non trainable and then set as trainable to compare the performance of the models.
-There are some visualization methods as well. Samples visualization was performed only with keras, to omit repetitions, yet augmentation visualization was performed using both keras and torch libraries.
-Optimal learning rate for all models was implemented to determine visually and to be set on user choice.(Leslie N. Smith method - https://arxiv.org/abs/1506.01186)
-All visualizations were stored to the corresponding folder(*project_folder/results/**library)
-*location of all of the files for the project
-**library which you are using - keras/torch
-'''
-
 
 class ImageClassificationTF:
     def __init__(self, path, image_size):
